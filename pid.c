9d0bf84f6594175cd1f9f13958bb263690c74855
caid10 <kingcaid101@gmail.com>
2023-05-24 11:59:07 -0700

    betty

--- pid.c
+++ pid.c
@@ -2,15 +2,30 @@
 #include <unistd.h>
 
 /**
- * main - PID
+ * add_numbers - Adds two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The sum of the two numbers.
+ */
+int add_numbers(int a, int b)
+{
+        return a + b;
+}
+
+/**
+ * main - Entry point of the program.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    pid_t my_pid;
+        int num1 = 5;
+        int num2 = 10;
+        int sum;
 
-    my_pid = getpid();
-    printf("%u\n", my_pid);
-    return (0);
+        sum = add_numbers(num1, num2);
+        printf("The sum is: %d\n", sum);
+
+        return 0;
 }
